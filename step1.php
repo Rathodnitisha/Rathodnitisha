fbf8f75fecd1e66cddae13d4520e2a8abc9119bb
Rathodnitisha <nitisharathod07@gmail.com>
2024-09-12 21:16:45 +0530

    Add files via upload

---
+++ step1.php
@@ -0,0 +1,15 @@
+<html>
+
+
+
+<form action="STEP.PHP" method="GET">
+   enter about me<input type="username"name="about"><br>
+   enter description<input type="email"name="em"><br>
+   enter projectdetail<input type="class"name="cl"><br>
+   enter task1<input type="php"name="ph"><br>
+   <input type="submit">
+  
+
+
+</form>
+</html>
